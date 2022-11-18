817202ce9759565359dd358f09216299649f3fc2
sawada_kyohei <khnm0322@gmail.com>
2022-11-19 00:26:12 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// ======
+// aos.js
+// ======
+AOS.init({
+  offset: 120,
+  delay: 8,
+  duration: 1000,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
